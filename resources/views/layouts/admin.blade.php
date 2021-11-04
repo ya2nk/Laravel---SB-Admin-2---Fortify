aa08767dfc662a58272938a33dc8d5b6b3a007c2
Jajang Umara <jajang.umara@gmail.com>
2021-11-04 16:37:26 +0700

    add assets

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -347,6 +347,8 @@
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('js/alpine-extends.js') }}" defer></script>
+<script src="{{ asset('js/alpine.min.js') }}" defer></script>
 @stack('js')
 </body>
 </html>
