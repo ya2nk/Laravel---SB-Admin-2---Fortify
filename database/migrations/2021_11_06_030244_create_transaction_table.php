28374b3a423d22941b0354c3557d091ddc913926
Jajang Umara <jajang.umara@gmail.com>
2021-11-06 12:57:57 +0700

    add migration

---
+++ database/migrations/2021_11_06_030244_create_transaction_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->integer("id_supplier");
+            $table->integer("ppn");
+            $table->integer("tanggal_jt");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
